471ded6044b1101d087efc775d3471576e149431
William Baxter <20.william.baxter@ksd.org>
2019-04-24 11:34:01 -0700

    Menus

---
+++ Sci_Fi/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenuButtons : MonoBehaviour {
+    public Dropdown drop;
+    public void NewGame()
+    {
+        if (drop.value == 1)
+        {
+            SceneManager.LoadScene("Level1H");
+        }
+        else
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+    public void NewHard()
+    {
+        SceneManager.LoadScene("Level1H");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
