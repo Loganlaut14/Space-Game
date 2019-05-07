aa84cc14aedf457cf4bf621116724d1c97321c90
Logan Lautenslager <20.logan.lautenslage@ksd.org>
2019-05-07 11:29:44 -0700

    Gun Update

--- Sci_Fi/Assets/GunScript.cs
+++ Sci_Fi/Assets/GunScript.cs
@@ -1,25 +1,75 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
 
 public class GunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    public Animator animator;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"));
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
         {
+
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1")&& Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
             
         }
     }
 
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading...", true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        animator.SetBool("Reloading...", false);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot ()
     {
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
@@ -30,6 +80,15 @@ public class GunScript : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
+
     }
 }
