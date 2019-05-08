694575794bb79a067b8b4e67c598298ada237ae8
Logan Lautenslager <20.logan.lautenslage@ksd.org>
2019-05-08 12:53:22 -0700

    Save Data

--- Sci_Fi/Assets/Player.cs
+++ Sci_Fi/Assets/Player.cs
@@ -6,4 +6,24 @@ public class Player : MonoBehaviour
 {
     public int level = 3;
     public int health = 40;
+
+    public void SavePlayer()
+    {
+        SavedSystem.SavePlayer(this);
+
+    }
+
+    public void LoadPlayer ()
+    {
+        PlayerData data = SavedSystem.LoadPlayer();
+
+        level = data.level;
+        health = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
 }
---
+++ Sci_Fi/Assets/Scripts/HP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour {
+
+    public int health = 10;
+    public Text healthText;
+    public Slider healthSlider;
+
+    void Start()
+    {
+        healthSlider.GetComponent<Slider>().value = health;
+        healthText.GetComponent<Text>().text = "Health" + health;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health--;
+            healthText.GetComponent<Text>().text = "Health" + health;
+            healthSlider.GetComponent<Slider>().value = health;
+        }
+        if(health <= 0)
+        {
+            //reload the level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+}
