3e74c4ecc93c67d5dcb1faf0a7cc417efe9ef5fa
William Baxter <20.william.baxter@ksd.org>
2019-05-16 13:31:52 -0700

    Level2

---
+++ Sci_Fi/Assets/Scripts/Lose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lose : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // attach to the object ending the game, not the player
+            SceneManager.LoadScene("Lose");
+        }
+
+    }
+
+}
